e0e4b7b63b6667a60bf8a1a059d28f32c91b3898
Nurik <nurik6@gmail.com>
2018-07-17 23:37:32 +0500

    fix(migrations): Fixed migrations rollback;

--- config/Migrations/20180716120412_DropDesirableAndundesirableColumnsFromApplicants.php
+++ config/Migrations/20180716120412_DropDesirableAndundesirableColumnsFromApplicants.php
@@ -10,11 +10,30 @@ class DropDesirableAndundesirableColumnsFromApplicants extends AbstractMigration
      * http://docs.phinx.org/en/latest/migrations.html#the-change-method
      * @return void
      */
-    public function change()
+    public function up()
     {
         $table = $this->table('applicants');
         $table->removeColumn('desirable_countries');
         $table->removeColumn('undesirable_countries');
+        $table->update();
+    }
+
+    public function down()
+    {
+        $table = $this->table('applicants');
+        $table->addColumn('desirable_countries','json',[
+            'null'=>true,
+            'default'=>null,
+            'comment'=>'Страны желательные к отбытию'
+        ]);
+        /* JSON */
+
+        $table->addColumn('undesirable_countries','json',[
+            'null'=>true,
+            'default'=>null,
+            'comment'=>'Страны нежелательные к отбытию'
+        ]);
+
         $table->update();
     }
 }
