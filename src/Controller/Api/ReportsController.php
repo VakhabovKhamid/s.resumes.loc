9a51bb43f1aaac2544a1acd9ca8133761a1f9775
Nurik <nurik6@gmail.com>
2018-07-20 22:25:06 +0500

    fix(reports): Fixed pivot reports with hard-coded query

--- src/Controller/Api/ReportsController.php
+++ src/Controller/Api/ReportsController.php
@@ -23,45 +23,102 @@ class ReportsController extends ApiController
         $lang = I18n::getLocale();
 
         $countryNameColumn = MappedLanguageHelper::get('name',$lang);
-        $countries = $conn->query("SELECT id, {$countryNameColumn} as name FROM dictionary_countries")->fetchAll(\PDO::FETCH_ASSOC);
-        $industries = $conn->query("SELECT id, {$countryNameColumn} as name FROM dictionary_industries")->fetchAll(\PDO::FETCH_ASSOC);
+        // $countries = $conn->query("SELECT id, {$countryNameColumn} as name FROM dictionary_countries")->fetchAll(\PDO::FETCH_ASSOC);
+        // $industries = $conn->query("SELECT id, {$countryNameColumn} as name FROM dictionary_industries")->fetchAll(\PDO::FETCH_ASSOC);
         
-        $countryCounterColumns = array_map(function($country){
-            $columnCounter = new ColumnCountryCounter();
-            return $columnCounter->make($country);
-        },$countries);
+        // $countryCounterColumns = array_map(function($country){
+        //     $columnCounter = new ColumnCountryCounter();
+        //     return $columnCounter->make($country);
+        // },$countries);
 
-        $industryCounterColumns = array_map(function($industry){
-            $columnCounter = new ColumnIndustryCounter();
-            return $columnCounter->make($industry);
-        },$industries);
+        // $industryCounterColumns = array_map(function($industry){
+        //     $columnCounter = new ColumnIndustryCounter();
+        //     return $columnCounter->make($industry);
+        // },$industries);
 
-        $dynamicColumns = new DynamicColumns();
-        $commonColumns = [
-            'dr.id',
-            'dr.name_ru_c as "region_name"',
-            "COUNT(a.id) as 'applicants'",
-            "SUM(IF(a.sex = 'M',1,0)) as 'M'",
-            "SUM(IF(a.sex = 'F',1,0)) as 'F'",
-        ];
-        $dynamicColumns->concat($commonColumns);
-        $dynamicColumns->concat($countryCounterColumns);
-        $dynamicColumns->concat($industryCounterColumns);
-        
-        $mainQuery = "
+        // $dynamicColumns = new DynamicColumns();
+        // $commonColumns = [
+        //     'dr.id',
+        //     'dr.name_ru_c as "region_name"',
+        //     "COUNT(a.id) as 'applicants'",
+        //     "SUM(IF(a.sex = 'M',1,0)) as 'M'",
+        //     "SUM(IF(a.sex = 'F',1,0)) as 'F'",
+        // ];
+        // $dynamicColumns->concat($commonColumns);
+        // $dynamicColumns->concat($countryCounterColumns);
+        $mainQuery = <<<SQL
             SELECT
-                {$dynamicColumns->render()}
+                dr.id,
+                $countryNameColumn as region_name,
+                COUNT(q1.id) as 'applicants',
+                SUM(IF(q1.sex = 'M',1,0)) as 'M',
+                SUM(IF(q1.sex = 'F',1,0)) as 'F',
+                SUM(q1.adc1) as 'Россия', 
+                SUM(q1.adc2) as 'Корея',
+                SUM(q1.adc3) as 'Казахстан', 
+                SUM(q1.adc4) as 'Турция', 
+                SUM(q1.adc6) as 'Япония', 
+                SUM(q1.adc9) as 'Страны ЕС', 
+                SUM(q1.adc10) as 'Америка',
+                SUM(q1.ai1) as 'Стройтельство',
+                SUM(q1.ai2) as 'Сфера услуг',
+                SUM(q1.ai3) as 'Общепит',
+                SUM(q1.ai4) as 'Торговля'
             FROM dictionary_regions dr
-            LEFT JOIN applicants a
-                ON dr.id = a.address_region_id
-            LEFT JOIN dictionary_countries dc
-                ON a.address_country_id = dc.id
-            LEFT JOIN applicant_desirable_countries adc
-                ON a.id = adc.applicant_id
-            LEFT JOIN applicant_industries ai
-                ON ai.applicant_id = a.id
+            JOIN (
+                SELECT 
+                a.*, 
+                ISNULL(adc1.id) as adc1, 
+                ISNULL(adc2.id) as adc2, 
+                ISNULL(adc3.id) as adc3, 
+                ISNULL(adc4.id) as adc4, 
+                ISNULL(adc6.id) as adc6, 
+                ISNULL(adc9.id) as adc9, 
+                ISNULL(adc10.id) as adc10,
+                ISNULL(ai1.id) as ai1,
+                ISNULL(ai2.id) as ai2,
+                ISNULL(ai3.id) as ai3,
+                ISNULL(ai4.id) as ai4
+                FROM applicants a
+                LEFT JOIN applicant_desirable_countries adc1 ON (a.id = adc1.`applicant_id` AND adc1.`dictionary_country_id` = 1)
+                LEFT JOIN applicant_desirable_countries adc2 ON (a.id = adc2.`applicant_id` AND adc2.`dictionary_country_id` = 2)
+                LEFT JOIN applicant_desirable_countries adc3 ON (a.id = adc3.`applicant_id` AND adc3.`dictionary_country_id` = 3)
+                LEFT JOIN applicant_desirable_countries adc4 ON (a.id = adc4.`applicant_id` AND adc4.`dictionary_country_id` = 4)
+                LEFT JOIN applicant_desirable_countries adc6 ON (a.id = adc6.`applicant_id` AND adc6.`dictionary_country_id` = 6)
+                LEFT JOIN applicant_desirable_countries adc9 ON (a.id = adc9.`applicant_id` AND adc9.`dictionary_country_id` = 9)
+                LEFT JOIN applicant_desirable_countries adc10 ON (a.id = adc10.`applicant_id` AND adc10.`dictionary_country_id` = 10)
+                LEFT JOIN applicant_industries ai1 ON (a.id = ai1.`applicant_id` AND ai1.`dictionary_industry_id` = 1)
+                LEFT JOIN applicant_industries ai2 ON (a.id = ai2.`applicant_id` AND ai2.`dictionary_industry_id` = 2)
+                LEFT JOIN applicant_industries ai3 ON (a.id = ai3.`applicant_id` AND ai3.`dictionary_industry_id` = 3)
+                LEFT JOIN applicant_industries ai4 ON (a.id = ai4.`applicant_id` AND ai4.`dictionary_industry_id` = 4)
+
+                GROUP BY a.id
+            ) as q1 ON q1.address_region_id = dr.id
             GROUP BY dr.id
-        ";
+
+SQL;
+        // $dynamicColumns->concat($industryCounterColumns);
+        
+        // $mainQuery = "
+        //     SELECT
+        //         {$dynamicColumns->render()}
+        //     FROM dictionary_regions dr
+        //     JOIN applicants a
+        //         ON dr.id = a.address_region_id
+        //     JOIN dictionary_countries dc
+        //         ON a.address_country_id = dc.id
+        //     JOIN (
+        //         SELECT dc.id,COUNT(*) as 'countries' 
+        //         FROM dictionary_countries dc 
+        //         JOIN applicant_desirable_countries adc
+        //             ON dc.id = adc.dictionary_country_id
+        //         GROUP BY dc.id
+        //     ) as q1 ON dc.id = q1.id
+
+        //     JOIN applicant_industries ai
+        //         ON ai.applicant_id = a.id
+        //     GROUP BY dr.id
+        // ";
 
         // $totalQuery = "
         //     SELECT
@@ -79,6 +136,7 @@ class ReportsController extends ApiController
         // ";
 
         $result = $conn->query($mainQuery)->fetchAll(\PDO::FETCH_ASSOC);
+        // dd($result);
         $result = array_map(function($item){
             $newItem = [];
             foreach($item as $name=>$value){
@@ -90,6 +148,8 @@ class ReportsController extends ApiController
             }
             return $newItem;
         },$result);
+        // dd($result);
+
         $createTemplate = function($rows){
             if(empty($rows)){
                 throw new \Exception('Rows cannot be empty');
