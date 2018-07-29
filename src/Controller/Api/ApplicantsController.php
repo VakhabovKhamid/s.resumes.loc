08ff31913e7d112f6aeb1472e33d6758e9da69f9
I-Iomer <homer13.06.90@gmail.com>
2018-07-29 17:57:39 +0500

    SUNDAY BUGFIX.

--- src/Controller/Api/ApplicantsController.php
+++ src/Controller/Api/ApplicantsController.php
@@ -4,9 +4,7 @@ namespace App\Controller\Api;
 use App\Controller\AppController;
 use Cake\I18n\Date;
 use Cake\ORM\Locator\TableLocator;
-use App\Lib\QueryFilter\RulesSet;
-use App\Lib\QueryFilter\Rules\AttributeRule;
-use App\Lib\QueryFilter\Rules\ValueRule;
+
 
 /**
  * Applicants Controller
@@ -33,50 +31,35 @@ class ApplicantsController extends ApiController
         
         $searchConditions = $this->Applicants->getSearchConditions($data);
         
-        $industriesRules = new RulesSet($data);
-        $industriesRules->add(new AttributeRule(
-            'ApplicantIndustries.dictionary_industry_id',
-            'IN',
-            'industry_id'
-        ));
-        $industriesRules = $industriesRules->get();
-        
-        $desirableCountriesRules = new RulesSet($data);
-        $desirableCountriesRules->add(new AttributeRule(
-            'ApplicantDesirableCountries.dictionary_country_id',
-            'IN',
-            'desirable_country_id'
-        ));
-        $desirableCountriesRules = $desirableCountriesRules->get();
-        
         $query = $this->Applicants->find()
             ->contain([
-                'DictionaryCountries',
+                //'DictionaryCountries',
                 'DictionaryRegions',
                 'DictionaryDistricts',
                 'DictionaryEducationLevels',
+                // 'ApplicantDocuments',
                 'DesirableCountries',
                 'Industries',
-                // 'ApplicantDocuments',
-                // 'UndesirableCountries',
+                //'UndesirableCountries',
                 'Users' => ['Tokens']
             ], true)
-            ->innerJoinWith('ApplicantIndustries',function($q) use (&$industriesRules){
-                if(count($industriesRules) > 0){
-                    $q->where($industriesRules);
+            ->innerJoinWith('ApplicantIndustries',function($q) use ($data){
+                if(!empty($data['industry_id'])){
+					$q->where(['dictionary_industry_id IN' => $data['industry_id']]);
                 }
                 return $q;
             })
-            ->innerJoinWith('ApplicantDesirableCountries',function($q) use(&$desirableCountriesRules){
-                if(count($desirableCountriesRules) > 0){
-                    $q->where($desirableCountriesRules);
+            ->innerJoinWith('ApplicantDesirableCountries',function($q) use($data){
+                if(!empty($data['desirable_country_id'])){
+                    $q->where(['dictionary_country_id IN' => $data['desirable_country_id']]);
                 }
                 return $q;
             })
             ->distinct()
             ->where($searchConditions);
-
+			
         $applicants = $this->paginate($query);
+		
         $this->set(compact('applicants'));
         $this->set('_serialize', ['applicants']);
     }
@@ -86,8 +69,9 @@ class ApplicantsController extends ApiController
         $this->response->withDisabledCache();
 
         $data = $this->request->getData();
+        
         $searchConditions = $this->Applicants->getSearchConditions($data);
-
+        
         $query = $this->Applicants->find()
             ->contain([
                 //'DictionaryCountries',
@@ -97,13 +81,26 @@ class ApplicantsController extends ApiController
                 'DesirableCountries',
                 'UndesirableCountries',
                 'Industries',
-                'ApplicantDocuments',
+                // 'ApplicantDocuments',
                 'Users' => ['Tokens']
-            ])
+            ], true)
+            ->innerJoinWith('ApplicantIndustries',function($q) use ($data){
+                if(!empty($data['industry_id'])){
+					$q->where(['dictionary_industry_id IN' => $data['industry_id']]);
+                }
+                return $q;
+            })
+            ->innerJoinWith('ApplicantDesirableCountries',function($q) use($data){
+                if(!empty($data['desirable_country_id'])){
+                    $q->where(['dictionary_country_id IN' => $data['desirable_country_id']]);
+                }
+                return $q;
+            })
+            ->distinct()
             ->where($searchConditions);
-
+			
         $applicants = $query->all();
-
+		
         $this->set(compact('applicants'));
         $this->set('_serialize', ['applicants']);
     }
